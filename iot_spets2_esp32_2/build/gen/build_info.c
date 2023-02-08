e60781ccad498e28c9c5c9fe06efcdd5161d3718
Selim Hjorthall <selimhjorthall@gmail.com>
2023-02-08 17:23:48 +0100

    Working mqtt buzzer

--- iot_spets2_esp32_2/build/gen/build_info.c
+++ iot_spets2_esp32_2/build/gen/build_info.c
@@ -1,4 +1,4 @@
 /* Auto-generated, do not edit. */
-const char *build_id = "20230207-094438";
-const char *build_timestamp = "2023-02-07T09:44:38Z";
+const char *build_id = "20230208-151856";
+const char *build_timestamp = "2023-02-08T15:18:56Z";
 const char *build_version = "1.0";
--- iot_spets2_esp32_2/fs/init.js
+++ iot_spets2_esp32_2/fs/init.js
@@ -27,49 +27,53 @@ GPIO.setup_output(PIN_LEDR, 0);
 
 //print("ADC enabled!", ADC.enable(PIN_ADC1));
 
-
-function i2c_function() {
-
-    let i2c_h = I2C.get(); // I2C handle
-    let write_1 = I2C.writeRegB(i2c_h, 0x70, 0x00, 0x51);
-    Sys.usleep(100 * 1000);
-    let reading_1 = I2C.readRegW(i2c_h, 0x70, 2);
-    print("Read: ",reading_1);
-    //let write_2 = I2C.writeRegB(i2c_h, 0x70, 0x00, 0x51);
-    //Sys.usleep(100 * 1000);
-    //let reading_2 = I2C.readRegB(i2c_h, 0x70, 3);
-    //print("Read: ",reading_2);
-    print("Write: ",write_1);
-    //print("Write: ",write_2);
-    //print("Who: " ,string);
-    ton = 2*reading_1;
+function clearLog(){
+	for (let i = 0; i < 30; i++) {
+		print("");
+	}
 }
 
+clearLog();
+print("test");
+
+MQTT.sub('group8', function(conn, topic, msg) {
+
+    print();
+    print('Recieved MQTT Message');
+	print('Topic:', topic, ', Message:', msg);
+
+    let decoded_msg = JSON.parse(msg);
+    let device = decoded_msg.device;
+    let tone = decoded_msg.tone;
+ 
+    piezo_buzzer(tone*10);
+
+}, null);
+
+GPIO.set_button_handler(PIN_BTN1, GPIO.PULL_UP, GPIO.INT_EDGE_ANY, 100, 
+	function(x) {
+		//Pull-up, 
+		if (!GPIO.read(x)){
+			//Check if Button 2 is also pressed
+			print('Btn 1 on');
+            piezo_buzzer(1000);
+		}
+	}, null);
 
 
-function piezo_buzzer(){
+function piezo_buzzer(tone){
 
-    let startTime = Timer.now();
-    let count = 0;
-    let array = [];
-
-    if(GPIO.read(PIN_BTN1) === 0)
-    {
-        while (startTime + 10 > Timer.now()) {
-            i2c_function();
-            PWM.set(PIN_PWM, ton, 0.5);
-            Sys.usleep(1000 * 250);
-            array[count] = ton;
-            count++;
-        }
-        PWM.set(PIN_PWM, 0, 0.5);
-        let text = JSON.stringify(array);
-        MQTT.pub('group8', text, 1);
-        //GPIO.write(PIN_BTN1, 1);
-
-    }
-
+    PWM.set(PIN_PWM, tone, 0.5);
+    Sys.usleep(1000 * 100);
+    PWM.set(PIN_PWM, tone, 0.0);
 }
 
-Timer.set(1000, Timer.REPEAT, piezo_buzzer, null);
-//Timer.set(500, Timer.REPEAT, i2c_function, null);
+//Timer.set(1000, Timer.REPEAT, piezo_buzzer, null);
+
+
+/*
+function min_timer_callback(){
+	// Rensa lite i loggen för läsbarhet
+		clearLog();
+}
+Timer.set(200, Timer.REPEAT, min_timer_callback, null);*/
