f11cdc79e8fc009b4b68c54e9f8bf99959f31e39
oscarjanze <oser0035@student.umu.se>
2023-03-01 12:30:06 +0100

    No man should have all that power

--- projekt/esp32_b/fs/init.js
+++ projekt/esp32_b/fs/init.js
@@ -15,6 +15,7 @@ let PIN_PWM = 17;
 let PIN_LEDR = 33; // red LED #1
 let PIN_ADC1 = 36;
 let PIN_ADC2 = 39;
+let PIN_MIC_GATE = 21;
 
 
 
@@ -23,16 +24,23 @@ print("ADC enabled! PIN_ADC1:", ADC.enable(PIN_ADC1));
 print("ADC enabled! PIN_ADC2:", ADC.enable(PIN_ADC2));
 
 
-let i = 0;
-let fan_hz = 25000;
-let fan_duty = 100;
-let test_mode = false;
+let count_increment_1 = 0;
+let count_increment_2 = 0;
 let light_array = [];
 let sound_array = [];
 let flicker_counter = 0;
 let diff = 0;
 //let store_val = 0;
+GPIO.set_mode(PIN_MIC_GATE, GPIO.MODE_INPUT);
 GPIO.setup_output(PIN_LEDR, 0);
+
+//              MÅL             //
+//              MQTT            //
+//  group8/esp32B/lightsensor   //
+//  group8/esp32B/movement      //
+//  group8/esp32B/mikrofon      //
+//  ADC * 2                     //
+//  read pin 1                  //
 
 
 
@@ -50,36 +58,33 @@ function verifyConnection(){
 
 
 
-//  MQTT
-//  group8/esp32B/lightsensor
-//  group8/esp32B/movement
-//  group8/esp32B/mikrofon
-
-
-//  ADC * 2
-//  read pin 1
 
 function ADC_function_1() {
     ADC.enable(PIN_ADC1);
     let adc_value_1 = ADC.read(PIN_ADC1);
-    light_array[i++] = adc_value_1;
+    light_array[count_increment_1++] = adc_value_1;
     !ADC.enable(PIN_ADC1);
 }
 
 function ADC_function_2() {
-    
-    //let adc_value_2 = ADC.read(PIN_ADC2);
-    //sound_array[i++] = adc_value_2;
+    ADC.enable(PIN_ADC2);
+    let adc_value_2 = ADC.read(PIN_ADC2);
+    //print("ADC2 ",adc_value_2);
+    sound_array[count_increment_2++] = adc_value_2;
+    !ADC.enable(PIN_ADC2);
 }
 
 
 //function Motion_detection() {}
 
 function check_flicker() {
-    for (let i = 0; i < light_array.length - 1; i++) {
-        diff = light_array[i] - light_array[i+1];
+    for (let i = 1; i < light_array.length-1; i++) {
+        //print("Whats this?: ",light_array[i], light_array[i-1]);
+        diff = light_array[i] - light_array[i-1];
+
+        //print("diff:",diff);
         if(light_array.length > 60){
-            if (30 < diff || -30 > diff) {
+            if (50 < diff || -50 > diff) {
                 flicker_counter++;
                 print("Counted a flick!");
             }
@@ -98,15 +103,21 @@ function print_array() {
     
     print("Here!");
     //print('Light array:', + light_array);
+    //for (let i = 0; i < sound_array.length; i++) {
+    //    print("Val sound:", sound_array[i]);
+    //}
     //for (let i = 0; i < light_array.length; i++) {
-    //    //print("Val:", light_array[i]);
+    //    print("Val light:", light_array[i]);
     //}
     print("Lenght of light_array:", light_array.length);
     print("Lenght of sound_array:", sound_array.length);
+    
     check_flicker();
     light_array = [];
     sound_array = [];
-    i = 0;
+    count_increment_1 = 0;
+    count_increment_2 = 0;
+
 }
 
 
